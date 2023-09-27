448a526b2af195377c54cf353395aca90176ca2e
raghubandreddi <75253716+raghubandreddi@users.noreply.github.com>
2023-09-27 16:31:41 +0530

    Update zoning-creation.sh

--- zoning-creation/zoning-creation.sh
+++ zoning-creation/zoning-creation.sh
@@ -5,7 +5,7 @@ done
 echo "============================================================" >> zoning-creation/output.txt
 
 cat zoning-creation/input.txt | while read column1 column2 column3 column4 ; do
-    echo "zonecreate "\"$column1\_$column3\", \"$column1\;$column3\""" >> zoning-creation/output.txt
+    echo "zonecreate "\"$column1\_TO_$column3\", \"$column1\;$column3\""" >> zoning-creation/output.txt
 done
 
 echo "============================================================" >> zoning-creation/output.txt
